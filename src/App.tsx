92afebed5cd184f4e8d18ef2e0a13f789607d729
Guy Balaam <guybal@amazon.com>
2018-05-27 18:56:52 +0100

    Initial ts react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <h1>React-Wars</h1>
+    );
+  }
+}
+
+export default App;
